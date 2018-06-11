0a77b53c7117608ae5c269a1ae95c7d4142f59ae
Thiago Marcolino <thiago.marcolino@farfetch.com>
2018-06-11 11:17:23 +0100

    first commit

---
+++ AutoResetEventConsoleApp/AutoResetEventConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoResetEventConsoleApp
+{
+    class Program
+    {
+        static AutoResetEvent autoResetEvent;
+
+        //static void Main(string[] args)
+        //{
+        //    autoResetEvent = new AutoResetEvent(false);
+
+        //    while (!autoResetEvent.WaitOne(TimeSpan.FromSeconds(1)))
+        //    {
+        //        Console.WriteLine("Continue");
+        //        StopEvent();
+        //    }
+
+        //    Console.WriteLine("Thread got signal");
+
+        //    Console.ReadKey();
+        //}
+
+        static void StopEvent()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            autoResetEvent.Set();
+        }
+    }
+}
---
+++ AutoResetEventConsoleApp/AutoResetEventConsoleApp/Program2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoResetEventConsoleApp
+{
+    class Program2
+    {
+        static AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+        static string dataFromServer = "none";
+
+        static void Main(string[] args)
+        {
+            Task task = Task.Factory.StartNew(() =>
+            {
+                GetDataFromServer();
+            });
+
+            //Put the current thread into waiting state until it receives the signal
+            while (!autoResetEvent.WaitOne(TimeSpan.FromSeconds(5)))
+            {
+                Console.WriteLine("passaram 5 s");
+                throw new Exception("kabummmm!!!");
+            }
+
+            //Thread got the signal
+            Console.WriteLine(dataFromServer);
+            Console.ReadKey();
+        }
+
+        static void GetDataFromServer()
+        {
+            //Calling any webservice to get data
+            Thread.Sleep(TimeSpan.FromSeconds(8));
+            Console.WriteLine("Esperei o server");
+            dataFromServer = "Webservice data";
+            autoResetEvent.Set();
+        }
+    }
+}
